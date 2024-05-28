2d177165113730864205bcbb4e72c74c2438c308
jlloydo430 <jesselloyd911@gmail.com>
2024-05-28 11:43:05 -0400

    api client refactor

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,8 @@
 // src/services/apiClient.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
 
 const apiClient = axios.create({
   baseURL: API_URL,
