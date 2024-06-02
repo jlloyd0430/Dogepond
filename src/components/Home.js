46d8710bbf37f7a4ac57277cc44638d35f50c314
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-06-02 16:45:19 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -108,7 +108,7 @@ const Home = () => {
             
       </div>
       <div className="main-content">
-            <p>we are currently testing backend api and things fornour bot so the images may not be showing at the moment for the older drops</p>
+            <p>we are currently testing backend api and things for our bot so the images may not be showing at the moment for the older drops</p>
         <div className="card">
           {error && <p>{error}</p>}
           {filteredDrops.length > 0 ? (
