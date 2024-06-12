e9d51b6ea9959adde383a2274c4945d7f223d971
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-06-12 01:49:47 -0400

    add telegram

--- src/components/discordBotInvite.js
+++ src/components/discordBotInvite.js
@@ -1,20 +1,47 @@
-// src/components/DiscordBotInvite.js
-import React from 'react';
-import './discordBotInvite.css'; // Create this CSS file for styles
-import botImage from './uploads/botimage.jpg'; // Ensure the path to your image is correct
+import React from "react";
+import "./discordBotInvite.css"; // Ensure this CSS file is created and correctly imported
+import discordBotImage from "./uploads/botimage.jpg"; // Ensure the path to your image is correct
+import telegramBotImage from "./uploads/telegrambot.webp"; // Ensure the path to your Telegram bot image is correct
 
 const DiscordBotInvite = () => {
-  const inviteLink = "https://discord.com/oauth2/authorize?client_id=1246157792096813137&permissions=2147483647&scope=bot%20applications.commands"; // Replace with your actual invite link
+  const discordInviteLink =
+    "https://discord.com/oauth2/authorize?client_id=1246157792096813137&permissions=2147483647&scope=bot%20applications.commands"; // Replace with your actual Discord invite link
+  const telegramInviteLink = "https://t.me/dogepondBot"; // Replace with your actual Telegram bot invite link
 
   return (
-    <div className="discord-bot-invite">
-      <a href={inviteLink} target="_blank" rel="noopener noreferrer">
-        <img src={botImage} alt="Invite Discord Bot" className="discord-bot-image" />
-      </a>
-      <p className='bio'>Invite Dogepond bot to your discord and recieve all the latest drops/updates!</p>
+    <div className="bot-invite-container">
+      <p className="invite-text">
+        Invite Dogepond bot(s) to your Discord & Telegram and receive all the
+        latest drops/updates!
+      </p>
+      <div className="bot-invites">
+        <div className="bot-invite">
+          <a href={discordInviteLink} target="_blank" rel="noopener noreferrer">
+            <img
+              src={discordBotImage}
+              alt="Invite Discord Bot"
+              className="bot-image"
+            />
+          </a>
+          <p>Discord Bot</p>
+        </div>
+        <div className="bot-invite">
+          <a
+            href={telegramInviteLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={telegramBotImage}
+              alt="Invite Telegram Bot"
+              className="bot-image"
+            />
+          </a>
+          <p>Telegram Bot</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default DiscordBotInvite;
-
