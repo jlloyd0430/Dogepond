2ca736503f05ca6f5c7325b943be6a1f8b2d63dd
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-07-05 14:51:30 -0400

    Add files via upload

---
+++ src/wallets/wallets.js
@@ -0,0 +1,70 @@
+import { getDogeLabsWalletAddress, sendDogeFromDogeLabs } from "./dogelabs";
+
+export const DOGELABS_WALLET = 'dogeLabs';
+
+export const PAYMENT_TYPE = 'payment';
+export const DOGINALS_TYPE = 'doginals';
+
+export const PAYLOAD_TYPES = {
+  text: 'PLAIN_TEXT',
+  base64: 'BASE_64'
+};
+
+export function defaultLogo(walletProvider) {
+  switch (walletProvider) {
+    case DOGELABS_WALLET:
+      return "https://drc-20.org/logo.svg";
+    default:
+      return undefined;
+  }
+}
+
+export async function getWalletAddress(walletProvider, walletType) {
+  switch (walletProvider) {
+    case DOGELABS_WALLET:
+      return await getDogeLabsWalletAddress(walletType);
+    default:
+      return '';
+  }
+}
+
+export async function signPsbt(walletProvider, psbtHex) {
+  switch (walletProvider) {
+    case DOGELABS_WALLET:
+      return await window.dogeLabs?.signPsbt(psbtHex);
+    default:
+      throw `PSBTs not supported for ${walletProvider}`;
+  }
+}
+
+export async function sendDoge(walletProvider, address, dogeAmount, originator) {
+  switch (walletProvider) {
+    case DOGELABS_WALLET:
+      return await sendDogeFromDogeLabs(dogeAmount, address, originator);
+    default:
+      throw `Sending DOGE not supported for ${walletProvider}`;
+  }
+}
+
+export async function directInscribe(walletProvider, contentType, payloadType, content, additionalFee, feeRate) {
+  switch (walletProvider) {
+    case DOGELABS_WALLET:
+    default:
+      throw `Direct inscriptions not supported for ${walletProvider}`;
+  }
+}
+
+export async function getWalletData(address) {
+  const response = await fetch(`https://dogeturbo.ordinalswallet.com/wallet/${address}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getWalletBalance(walletProvider) {
+  switch (walletProvider) {
+    case DOGELABS_WALLET:
+      return await window.dogeLabs?.getBalance();
+    default:
+      throw `Getting balance not supported for ${walletProvider}`;
+  }
+}
