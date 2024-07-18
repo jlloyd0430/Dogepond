9491ed84359e5a4356e46271eb55c1a8329812ff
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-07-17 21:14:06 -0400

    Update Fish.js

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Fish.css'
+import AdBannerCarousel from "../components/AdBannerCarousel";
 
 const Fish = () => {
   return (
+     <AdBannerCarousel /> 
     <div className='container'>
       <h1>Fish!</h1>
+    
       <p>Welcome.... Fishing coming soon.....$kwak</p>
     </div>
   );
