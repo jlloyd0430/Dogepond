237e7852bd6e9091fc301d9f5b5e79b9f94b46a5
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-07-27 16:14:55 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { AuthContext } from "../context/AuthContext";
 import NFTCard from "../components/NFTCard";
 import apiClient from "../services/apiClient";
 import AdBannerCarousel from "../components/AdBannerCarousel";
-import DiscordBotInvite from "../components/discordBotInvite"; // Corrected component name case
+import DiscordBotInvite from "../components/discordBotInvite";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import Papa from 'papaparse';
@@ -24,6 +24,7 @@ const Home = () => {
   const [snapshotData, setSnapshotData] = useState([]);
   const [drc20Ticker, setDrc20Ticker] = useState("");
   const [drc20SnapshotData, setDrc20SnapshotData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,11 +37,9 @@ const Home = () => {
             }
           : {};
         const result = await apiClient.get("/nftdrops/approved", config);
-        console.log("Fetched approved NFT drops:", result.data);
         setApprovedDrops(result.data);
         setError("");
       } catch (error) {
-        console.error("Error fetching approved NFT drops:", error);
         setError("Failed to fetch approved NFT drops. Please try again later.");
       }
     };
@@ -69,7 +68,6 @@ const Home = () => {
       filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
     }
 
-    console.log("Filtered drops:", filtered);
     setFilteredDrops(filtered);
   };
 
@@ -80,17 +78,13 @@ const Home = () => {
           "x-auth-token": auth.token,
         },
       };
-      console.log("Sending like request for NFT Drop ID:", id);
       const response = await apiClient.post(`/nftdrops/${id}/like`, {}, config);
-      console.log("Like response:", response.data);
       setApprovedDrops((prevDrops) =>
         prevDrops.map((drop) => (drop._id === id ? response.data : drop))
       );
     } catch (error) {
       if (error.response && error.response.data.msg) {
         alert(error.response.data.msg);
-      } else {
-        console.error("Error liking drop:", error);
       }
     }
   };
@@ -100,13 +94,11 @@ const Home = () => {
   };
 
   const handleFilterChange = (filterValue) => {
-    console.log(`Changing filter to: ${filterValue}`);
     setFilter(filterValue);
     setShowDropdown(false);
   };
 
   const handleDropTypeChange = (type) => {
-    console.log(`Changing drop type to: ${type}`);
     setDropType(type);
     setShowDropTypeDropdown(false);
   };
@@ -131,34 +123,44 @@ const Home = () => {
   };
 
   const fetchDrc20Snapshot = async () => {
-    let allData = [];
-    let page = 0;
-    const limit = 100; // Adjust as per API limits
-
+    setLoading(true);
     try {
-      while (true) {
-        const response = await fetch(`https://xdg-mainnet.gomaestro-api.org/v0/assets/drc20/${drc20Ticker}/holders?limit=${limit}&page=${page}`, {
+      let allData = [];
+      let page = 0;
+      let hasMoreData = true;
+
+      while (hasMoreData) {
+        const response = await fetch(`https://xdg-mainnet.gomaestro-api.org/v0/assets/drc20/${drc20Ticker}/holders?limit=100&page=${page}`, {
           headers: {
             'Accept': 'application/json',
             'api-key': process.env.REACT_APP_API_KEY
           }
         });
+
+        if (response.status === 429) {
+          // Rate limit exceeded, wait for a while before retrying
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          continue;
+        }
+
         const data = await response.json();
 
-        if (data.data.length === 0) break; // No more data
-
-        allData = allData.concat(data.data.map(item => ({
-          address: item.address,
-          balance: item.balance
-        })));
-
-        page++;
+        if (data.data && data.data.length > 0) {
+          allData = allData.concat(data.data.map(item => ({
+            address: item.address,
+            balance: item.balance
+          })));
+          page++;
+        } else {
+          hasMoreData = false;
+        }
       }
 
       setDrc20SnapshotData(allData);
     } catch (error) {
       console.error('Failed to fetch DRC-20 snapshot data:', error);
     }
+    setLoading(false);
   };
 
   const exportToCSV = () => {
@@ -280,7 +282,9 @@ const Home = () => {
               value={drc20Ticker}
               onChange={(e) => setDrc20Ticker(e.target.value)}
             />
-            <button onClick={fetchDrc20Snapshot}>Snap!t</button>
+            <button onClick={fetchDrc20Snapshot} disabled={loading}>
+              {loading ? 'Loading...' : 'Snap!t'}
+            </button>
             {drc20SnapshotData.length > 0 && (
               <div className="snapshot-results">
                 <h4>Snapshot Results</h4>
