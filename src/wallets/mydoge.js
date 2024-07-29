eb9a406ea5e51fcca4c998bd381570dfbd390c4f
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-07-28 22:53:50 -0400

    Update mydoge.js

--- src/wallets/mydoge.js
+++ src/wallets/mydoge.js
@@ -1,20 +1,20 @@
 export async function getMyDogeWalletAddress(walletType) {
-  if (typeof window.doge === 'undefined' || !window.doge.isMyDoge) {
+  if (typeof window.myDoge === 'undefined') {
     throw new Error('MyDoge Wallet is not installed');
   }
   try {
-    const accounts = await window.doge.requestAccounts();
+    const accounts = await window.myDoge.requestAccounts();
     if (accounts.length !== 1) {
       throw new Error(`Invalid number of accounts detected (${accounts.length})`);
     }
     return accounts[0];
   } catch (err) {
-    throw new Error('User did not grant access to MyDoge Wallet');
+    throw new Error('User did not grant access to MyDoge');
   }
 }
 
 export async function sendDogeFromMyDoge(amount, address) {
-  const txid = await window.doge?.sendBitcoin(address, amount);
+  const txid = await window.myDoge?.sendBitcoin(address, amount);
   return txid;
 }
 
@@ -22,11 +22,4 @@ export async function getWalletDataFromMyDoge(address) {
   const response = await fetch(`https://dogeturbo.ordinalswallet.com/wallet/${address}`);
   const data = await response.json();
   return data;
-}
-
-export async function getMyDogeBalance() {
-  if (typeof window.doge === 'undefined' || !window.doge.isMyDoge) {
-    throw new Error('MyDoge Wallet is not installed');
-  }
-  return await window.doge.getBalance();
 }
