88d35362a1821e7c8d052cea7f02d8ca7feba6f3
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-09 01:37:33 -0400

    Update TrendingDunes.js

--- src/components/TrendingDunes.js
+++ src/components/TrendingDunes.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import cheerio from "cheerio";
-import "./Trending.css";
-import DuneForm from "./Duneform";
-import { submitOrder, checkOrderStatus } from '../services/duneApiClient';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilter, faSort } from "@fortawesome/free-solid-svg-icons"; // Import the FontAwesome icons
-
+import "./Trending.css"; // Add appropriate styles
+import DuneForm from "./Duneform"; // Import the form component
+import { submitOrder, checkOrderStatus } from '../services/duneApiClient'; // Import the Dune API functions
 const TrendingDunes = () => {
   const [dunes, setDunes] = useState([]);
   const [error, setError] = useState("");
@@ -14,30 +11,26 @@ const TrendingDunes = () => {
   const [walletDunes, setWalletDunes] = useState([]);
   const [balanceError, setBalanceError] = useState("");
   const [sortOrder, setSortOrder] = useState("mostRecent");
-  const [searchQuery, setSearchQuery] = useState(""); // Add search query state
-  const [view, setView] = useState("dunes");
-  const [showSortDropdown, setShowSortDropdown] = useState(false);
-
+  const [searchTerm, setSearchTerm] = useState(""); // State for search term
+  const [paymentInfo, setPaymentInfo] = useState(null);
+  const [orderStatus, setOrderStatus] = useState("");
+  const [view, setView] = useState("dunes"); // State to control which view is active
   useEffect(() => {
     const fetchTrendingDunes = async () => {
       try {
         const response = await axios.get("https://ord.dunesprotocol.com/dunes");
         const htmlData = response.data;
-
         const $ = cheerio.load(htmlData);
         const duneList = [];
-
         $("ul > li > a").each((index, element) => {
           const duneName = $(element).text();
           const duneLink = $(element).attr("href");
-
           duneList.push({
             name: duneName,
             link: `https://ord.dunesprotocol.com${duneLink}`,
-            index,
+            index, // Store the index to use for sorting
           });
         });
-
         setDunes(duneList);
       } catch (error) {
         console.error("Error fetching trending dunes:", error);
@@ -46,37 +39,34 @@ const TrendingDunes = () => {
     };
     fetchTrendingDunes();
   }, []);
-
-  const handleSearchChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const filteredDunes = dunes.filter((dune) =>
-    dune.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  const handleSortOrderChange = (order) => {
-    setSortOrder(order);
-    setShowSortDropdown(false);
-  };
-
-  const sortedDunes = [...filteredDunes].sort((a, b) => {
-    if (sortOrder === "mostRecent") {
-      return b.index - a.index;
-    }
-    return a.index - b.index;
-  });
-
   const handleWalletAddressChange = (e) => {
     setWalletAddress(e.target.value);
+  };
+  const encodeDuneName = (duneName) => {
+    return duneName.split(' ').join('%E2%80%A2');
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    const filteredDunes = dunes.filter(dune =>
+      dune.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setDunes(filteredDunes);
   };
 
   const handleFetchBalance = async () => {
     if (!walletAddress) {
       setBalanceError("Please enter a wallet address.");
+
+    
+        
+ const TrendingDunes = () => {
+  
       return;
     }
-
     try {
       const response = await axios.get(`https://wonky-ord.dogeord.io/dunes/balance/${walletAddress}?show_all=true`);
       setWalletDunes(response.data.dunes);
@@ -86,12 +76,24 @@ const TrendingDunes = () => {
       setBalanceError("Failed to fetch dunes balance. Please try again later.");
     }
   };
+  const handleSortOrderChange = (e) => {
+    setSortOrder(e.target.value);
+  };
 
+  const sortedDunes = [...dunes].sort((a, b) => {
+    if (sortOrder === "mostRecent") {
+      return b.index - a.index; // Sort by most recent
+    }
+
+ const TrendingDunes = () => {
+  
+    return a.index - b.index; // Sort by oldest
+  });
   const handleSubmit = async (formData) => {
     try {
       const result = await submitOrder(formData);
       setPaymentInfo({ dogeAmount: result.dogeAmount, address: result.address, index: result.index });
-
+      // Periodically check the order status
       const intervalId = setInterval(async () => {
         try {
           const status = await checkOrderStatus(result.index);
@@ -102,20 +104,18 @@ const TrendingDunes = () => {
         } catch (error) {
           console.error('Error checking order status:', error);
         }
-      }, 30000);
+      }, 30000); // Check every 30 seconds
     } catch (error) {
       console.error('Error:', error);
       alert('An error occurred. Please try again.');
     }
   };
-
   return (
     <div className="trending-container">
       <div className="trending-header-container">
         <h1 className="trending-ttitle" onClick={() => setView("dunes")}>All Dunes</h1>
         <h1 className="trending-ttitle" onClick={() => setView("etcher")}>Etcher</h1>
       </div>
-
       {view === "dunes" && (
         <>
           {error && <p className="trending-error">{error}</p>}
@@ -130,47 +130,31 @@ const TrendingDunes = () => {
           </div>
 
           {balanceError && <p className="trending-error">{balanceError}</p>}
-          <div className="trending-wallet-dunes-list">
-            {walletDunes.map((dune, index) => (
-              <div key={index} className="trending-wallet-dune-card">
-                <p>{dune.dune} ({dune.symbol}): {dune.total_balance / (10 ** dune.divisibility)} {dune.symbol}</p>
-              </div>
-            ))}
+          <div className="trending-sort-container">
+            <label htmlFor="sortOrder">Sort by:</label>
+            <select id="sortOrder" value={sortOrder} onChange={handleSortOrderChange}>
+              <option value="mostRecent">Most Recent</option>
+              <option value="oldest">Oldest</option>
+            </select>
           </div>
 
-          <div className="trending-sort-search-container">
-            <div className="trending-sort-container">
-              <FontAwesomeIcon
-                icon={faSort}
-                className="trending-sort-icon"
-                onClick={() => setShowSortDropdown(!showSortDropdown)}
-              />
-              {showSortDropdown && (
-                <div className="trending-sort-dropdown">
-                  <div onClick={() => handleSortOrderChange("mostRecent")}>
-                    Most Recent
-                  </div>
-                  <div onClick={() => handleSortOrderChange("oldest")}>
-                    Oldest
-                  </div>
-                </div>
-              )}
-            </div>
-
-            <div className="trending-search-container">
-              <input
-                type="text"
-                placeholder="Search Dunes..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-                className="trending-search-input"
-              />
-              <button className="trending-search-button">Search</button>
-            </div>
+          <div className="trending-search-container">
+            <input
+              type="text"
+              placeholder="Search Dunes..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="trending-search-input"
+            />
+            <button className="trending-search-button" onClick={handleSearch}>
+              <img src="/path/to/your/search-icon.png" alt="Search" className="trending-search-icon" />
+            </button>
           </div>
 
           <div className="trending-dune-list">
-            {sortedDunes.map((dune, index) => (
+
+              
+              {sortedDunes.map((dune, index) => (
               <div key={index} className="trending-dune-card">
                 <a href={dune.link} target="_blank" rel="noopener noreferrer">
                   <h2>{dune.name}</h2>
@@ -180,14 +164,20 @@ const TrendingDunes = () => {
           </div>
         </>
       )}
-
       {view === "etcher" && (
         <div className="trending-form-container">
           <DuneForm onSubmit={handleSubmit} />
+          {paymentInfo && (
+            <div className="trending-payment-popup">
+              <p>Please send {paymentInfo.dogeAmount} DOGE to the following address:</p>
+              <p>{paymentInfo.address}</p>
+              <button onClick={() => navigator.clipboard.writeText(paymentInfo.address)}>Copy Address</button>
+              {orderStatus && <p>Order Status: {orderStatus}</p>}
+            </div>
+          )}
         </div>
       )}
     </div>
   );
 };
-
 export default TrendingDunes;
