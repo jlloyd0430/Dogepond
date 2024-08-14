670341bd8f040134f71bc3741127dd2dd5e41855
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-14 11:28:50 -0400

    Update duneApiClient.js

--- src/services/duneApiClient.js
+++ src/services/duneApiClient.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-
 const apiClient = axios.create({
-  baseURL: 'https://form.dogepond.com',  // Ensure your server is reachable
+  baseURL: 'https://form.dogepond.com',  // Updated to use your domain
   headers: {
     'Content-Type': 'application/json'
   }
 });
-
 export const submitOrder = async (orderData) => {
   try {
     const response = await apiClient.post('/order', orderData);
@@ -17,12 +15,14 @@ export const submitOrder = async (orderData) => {
   }
 };
 
-export const checkOrderStatus = async (orderId) => {
+export const checkOrderStatus = async (index) => {
   try {
-    const response = await apiClient.get(`/order/status/${orderId}`);
+    const response = await apiClient.get(`/order/status/${index}`);
     return response.data.status;
   } catch (error) {
     console.error('Error checking order status:', error);
-    throw new Error('An error occurred while checking the order status');
+
+    
+throw new Error('An error occurred while checking the order status');
   }
 };
