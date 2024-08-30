a11d7b1ef41422702000a8b288532ac043885be0
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-30 14:51:05 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,7 @@ const Header = () => {
           <>
             <Link className="nav-link" to="/post">Post</Link>
             <Link className="nav-link" to="/proposals">Vote</Link>
+            <Link className="nav-link" to="/mint">Mint</Link> {/* Add this link */}
             <ProfileDropdown />
           </>
         ) : (
@@ -24,6 +25,7 @@ const Header = () => {
             <Link className="nav-link" to="/login">Login</Link>
             <Link className="nav-link" to="/signup">Signup</Link>
             <Link className="nav-link" to="/packages">Services</Link>
+            <Link className="nav-link" to="/mint">Mint</Link> {/* Add this link for non-authenticated users as well */}
           </>
         )}
       </nav>
