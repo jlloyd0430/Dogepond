4c376469339ed8d1a9566a0b04f7fb647695390d
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-30 16:44:45 -0400

    Update Mint.js

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -12,7 +12,7 @@ const Mint = () => {
       <h2 className="section-title">Minting Live</h2>
       <div className="mint-section">
         <MintCard
-          image="../../public/peng.webp" // Image from the public folder
+          image="/peng.webp" // Image from the public folder
           title="Doginal Pengz"
           description="Doginal Pengz is a collection of 4200 penguins on DOGE 2100 supply is minting with dogepond"
           Supply="2100"
