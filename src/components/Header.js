76c7c9b670e62dc657cc76a0294d9a3c701bb11c
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-30 17:54:08 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import ProfileDropdown from './ProfileDropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 const Header = () => {
-  
   const { auth } = useContext(AuthContext);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
   return (
     <header className="header">
       <Link className="title" to="/">
@@ -30,11 +31,10 @@ const Header = () => {
               <Link className="nav-link" to="/post">Post</Link>
               <Link className="nav-link" to="/proposals">Vote</Link>
               <Link className="nav-link" to="/mint">Mint</Link>
+              <ProfileDropdown /> {/* Show the Profile Dropdown when logged in */}
             </>
           ) : (
             <>
-        const Header = () => {
-  
               <Link className="nav-link" to="/login">Login</Link>
               <Link className="nav-link" to="/signup">Signup</Link>
               <Link className="nav-link" to="/packages">Services</Link>
@@ -42,11 +42,16 @@ const Header = () => {
             </>
           )}
         </div>
-        <ProfileDropdown />
+        {auth.isAuthenticated && (
+          <FontAwesomeIcon
+            icon={faUserCircle}
+            className="profile-icon"
+            onClick={toggleMobileMenu}
+          />
+        )}
       </nav>
     </header>
   );
-
-  
 };
+
 export default Header;
