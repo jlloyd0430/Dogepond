046150310be23da1b418bba7733f37dffeaa7be0
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-30 17:40:40 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import ProfileDropdown from './ProfileDropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 const Header = () => {
   const { auth } = useContext(AuthContext);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <header className="header">
@@ -13,21 +20,28 @@ const Header = () => {
         <h1>DogePond</h1>
       </Link>
       <nav className="nav">
-        {auth.isAuthenticated ? (
-          <>
-            <Link className="nav-link" to="/post">Post</Link>
-            <Link className="nav-link" to="/proposals">Vote</Link>
-            <Link className="nav-link" to="/mint">Mint</Link> {/* Add this link */}
-            <ProfileDropdown />
-          </>
-        ) : (
-          <>
-            <Link className="nav-link" to="/login">Login</Link>
-            <Link className="nav-link" to="/signup">Signup</Link>
-            <Link className="nav-link" to="/packages">Services</Link>
-            <Link className="nav-link" to="/mint">Mint</Link> {/* Add this link for non-authenticated users as well */}
-          </>
-        )}
+        <FontAwesomeIcon
+          icon={faBars}
+          className="mobile-menu-icon"
+          onClick={toggleMobileMenu}
+        />
+        <div className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
+          {auth.isAuthenticated ? (
+            <>
+              <Link className="nav-link" to="/post">Post</Link>
+              <Link className="nav-link" to="/proposals">Vote</Link>
+              <Link className="nav-link" to="/mint">Mint</Link>
+            </>
+          ) : (
+            <>
+              <Link className="nav-link" to="/login">Login</Link>
+              <Link className="nav-link" to="/signup">Signup</Link>
+              <Link className="nav-link" to="/packages">Services</Link>
+              <Link className="nav-link" to="/mint">Mint</Link>
+            </>
+          )}
+        </div>
+        <ProfileDropdown />
       </nav>
     </header>
   );
