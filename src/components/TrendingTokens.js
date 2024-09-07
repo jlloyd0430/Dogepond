b46d4c7b47c54745124e6a884d16a3f31502d8e3
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-09-07 09:39:55 -0400

    Update TrendingTokens.js

--- src/components/TrendingTokens.js
+++ src/components/TrendingTokens.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Trending.css"; // Add appropriate styles
+
 const TrendingTokens = () => {
   const [tokens, setTokens] = useState([]);
   const [error, setError] = useState("");
+
   useEffect(() => {
     const fetchTrendingTokens = async () => {
       try {
@@ -21,33 +23,38 @@ const TrendingTokens = () => {
     return volume ? (volume / 1e8).toFixed(2) : "0.00"; // Convert satoshis to DOGE and format to 2 decimals
   };
 
+  const formatFloorPrice = (floorPrice) => {
+    return floorPrice ? (floorPrice / 1e8).toFixed(2) : "N/A"; // Convert satoshis to DOGE
+  };
+
   const renderChange = (change) => {
     const className = change >= 0 ? "positive-change" : "negative-change";
     return change !== undefined ? <span className={className}>{change.toFixed(2)}%</span> : <span>-</span>;
   };
 
   return (
-
-
     <div className="trending-container">
       <h1 className="trending-ttitle">Trending Tokens</h1>
       {error && <p className="trending-error">{error}</p>}
       <div className="trending-drc20-list">
         {tokens.map((token, index) => (
           <div key={index}>
-             <h2>{token.tick}</h2>
-             <div className="trending-drc20-card">
-               <img src={token.pic} alt={token.tick} />
-               <div className="trending-drci">
-                 <p>24h Volume: {formatVolume(token.volume24h)} DOGE</p>
-                 <p>24h Trades: {token.trades24h}</p>
-                 <p>24h Change: {renderChange(token.change24h)}</p>
-               </div>
-             </div>
+            <h2>{token.tick}</h2>
+            <div className="trending-drc20-card">
+              <img src={token.pic} alt={token.tick} />
+              <div className="trending-drci">
+                <p>24h Volume: {formatVolume(token.volume24h)} DOGE</p>
+                <p>24h Trades: {token.trades24h}</p>
+                <p>24h Change: {renderChange(token.change24h)}</p>
+                <p>Owners: {token.owners !== undefined ? token.owners : "N/A"}</p>
+                <p>Floor Price: {formatFloorPrice(token.floorPrice)} DOGE</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 export default TrendingTokens;
