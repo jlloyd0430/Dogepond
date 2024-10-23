8d265ddde59e21e73d5651ee5a953750baaea551
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-10-23 11:04:14 -0400

    Create Raffles.js

---
+++ src/components/Raffles.js
@@ -0,0 +1,13 @@
+// components/Raffles.js
+import React from 'react';
+
+const Raffles = () => {
+  return (
+    <div>
+      <h1>Raffles Page</h1>
+      <p>Welcome to the Raffles page. Here you will be able to view and participate in raffles tba</p>
+    </div>
+  );
+};
+
+export default Raffles;
